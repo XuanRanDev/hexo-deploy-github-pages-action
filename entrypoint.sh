0a29ae77c0fbdcfbc4701f5d1f86a71d93a806ae
XuanRan <59474133+XuanRanDev@users.noreply.github.com>
2022-12-29 15:47:38 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,7 +36,7 @@ echo ">>>>> Start deploy to ${TARGET_REPOSITORY} <<<<<"
 # Installs Git.
 echo ">>> Install Git ..."
 apt-get update && \
-apt-get install -y git && \
+apt-get install -y git=2.26.2 && \
 
 # Directs the action to the the Github workspace.
 cd "${GITHUB_WORKSPACE}"
